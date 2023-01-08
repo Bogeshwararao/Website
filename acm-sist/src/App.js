597542364e5155f91ffba5d5f86aa72e0a886a17
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-01-08 12:12:54 +0630

    commit

--- acm-sist/src/App.js
+++ acm-sist/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 //Components Imports section
 import Navbar from "./Components/Navbar.js";
@@ -14,22 +14,19 @@ import About from "./Screens/About";
 import Blogs from "./Screens/Bolgs";
 import Projects from "./Screens/Projects";
 import Contact from "./Screens/Contact";
-import { Navigate } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-     <Navigate>
-        <Route exact path="/" component={Home} />
-       <Route exact path="/About" component={About} />
-       <Route exact path="/Projects" component={Projects} />
-        <Route exact path="/Blogs" component={Blogs} />
-        <Route exact path="/Contact" component={Contact} />
-        <Navigate to="/" />
-      
-            </Navigate>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Blogs" element={<Blogs/>}/>
+      <Route path="/Projects" element={<Projects/>}/>
+      <Route path="/Contact" element={<Contact/>}/>
+     </Routes>
       {/* <Header /> */}
       {/* <h2>Let's do this...</h2> */}
     </div>
