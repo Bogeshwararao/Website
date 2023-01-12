5a7d616fef321560a16ca5693c00162f5c81a5f1
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-01-12 16:40:26 +0630

    commit

--- acm/src/Components/Navbar.js
+++ acm/src/Components/Navbar.js
@@ -5,7 +5,7 @@ import { BrowserRouter ,
 Routes,
 Route,
 Link } from "react-router-dom";
-
+import ACMlogo from "../Assets/acmnav.png";
 import  Home from "../Screens/Home";
 import About from "../Screens/About";
 import Team from "../Screens/Team";
@@ -18,17 +18,20 @@ function CollapsibleExample() {
   return (
     <BrowserRouter>
     <>
-    <Navbar collapseOnSelect expand="lg">
+    <Navbar collapseOnSelect expand="sm">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={ACMlogo} alt="" className="acmnavlogo" /></Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav  >
-            <Nav.Link as={Link} to="/" >Home</Nav.Link>
+        <Navbar.Collapse id="responsive-navbar-nav" className="hidden-navbar">
+          <Nav  id='total-nav-items'>
+           
+            <Nav.Link id="homelink" as={Link} to="/" >Home</Nav.Link>
             <Nav.Link as={Link} to="/About" >About</Nav.Link>
             <Nav.Link as={Link} to="/Team">Team</Nav.Link>
             <Nav.Link as={Link} to="/Event" > Event</Nav.Link>
             <Nav.Link as={Link} to="/Contact" > Contact us</Nav.Link>
+
          </Nav>
         </Navbar.Collapse>
       </Container>
